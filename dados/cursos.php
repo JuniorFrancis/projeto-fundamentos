48872e6b8790e6322fb19386a81053bb8667f36e
Antonio Junior Francis da Silva <juniorfrancis91@gmail.com>
2020-11-05 06:03:43 -0300

    Subindo arquivos para repositório na nuvem

---
+++ dados/cursos.php
@@ -0,0 +1,25 @@
+<?php 
+
+function cursos() 
+{
+    return [
+        "php" => [
+            "nomeCurso" => "Iniciando PHP",
+            "duracaoCurso" => 40,
+            "versaoFerramenta" => 7.4,
+            "statusCurso" => true
+        ],
+        "C#" => [
+            "nomeCurso" => "C# entre linhas",
+            "duracaoCurso" => 80,
+            "versaoFerramenta" => 8.1,
+            "statusCurso" => true
+        ],         
+        "java" => [
+            "nomeCurso" => "SoftBlue - JAVA",
+            "duracaoCurso" => 120,
+            "versaoFerramenta" => 15.4,
+            "statusCurso" => true
+          ]
+    ];
+}
